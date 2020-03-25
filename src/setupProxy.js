d6a442dbdae888f1bb68ce4e8415836a5ef02700
Terri Chu <terri.chu.dev@gmail.com>
2020-03-25 19:59:29 -0400

    fix proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,9 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy(["/api"], { target: "http://localhost:5000" }));
+  app.use(
+    createProxyMiddleware(["/api"], {
+      target: "https://save-atl-faves-api.herokuapp.com"
+    })
+  );
 };
