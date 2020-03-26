e774deb10e6e1e03052131b7eb792315ab2fe4e2
Terri Chu <terri.chu.dev@gmail.com>
2020-03-26 12:57:06 -0400

    new places

--- src/CityData/Places.js
+++ src/CityData/Places.js
@@ -1,11 +1,4 @@
-const AtlPlaces = [
-  {
-    name: "Cooks & Soldiers",
-    address: "691 14th St NW, Atlanta",
-    key: "ChIJLTcZFewE9YgRHsW7wP1bEYs",
-    image_attribution:
-      "['<a href=\"https://maps.google.com/maps/contrib/118337154225877634384\">Cooks &amp; Soldiers</a>']"
-  },
+const SFPlaces = [
   {
     name: "Public School 404",
     address: "930 Howell Mill Rd, Atlanta",
@@ -28,18 +21,60 @@ const AtlPlaces = [
       "['<a href=\"https://maps.google.com/maps/contrib/108513545042510219877\">Alex Moffitt</a>']"
   },
   {
+    name: "Bold Monk Brewing Co.",
+    address: "1737 Ellsworth Industrial Blvd NW suite d-1, Atlanta",
+    key: "ChIJT9KvknAF9YgReD6o6LCcvrk",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/103088049248843583183\">Candice Boothe</a>']"
+  },
+  {
+    name: "Food Terminal",
+    address: "1000 Marietta St NW UNIT 202, Atlanta",
+    key: "ChIJX7w9HaEF9YgRPR83q-zJWis",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113930873037724142825\">Torrey Smith</a>']"
+  },
+  {
+    name: "Bocado",
+    address: "887 Howell Mill Rd NW, Atlanta",
+    key: "ChIJp1zzA5ME9YgRAnTbCVEQNJo",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/100165444562403253362\">Mark McElroy</a>']"
+  },
+  {
+    name: "Atlanta Brewing Company",
+    address: "2323 Defoor Hills Rd NW, Atlanta",
+    key: "ChIJlzkMXDIF9YgRLRJaA2-T5aQ",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113040785979078086482\">Atlanta Brewing Company</a>']"
+  },
+  {
+    name: "Bottle Rocket Fine Food and Beverage",
+    address: "180 Walker St SW, Atlanta",
+    key: "ChIJJQEcinkD9YgRw46X2vrQ7rA",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/117034873783845675454\">Bottle Rocket Fine Food and Beverage</a>']"
+  },
+  {
+    name: "Manuel's Tavern",
+    address: "602 North Highland Avenue Northeast, Atlanta",
+    key: "ChIJnbi7oqcG9YgRCp6paNceye4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/114422700404552250864\">Mary C Pindar</a>']"
+  },
+  {
+    name: "Empire State South",
+    address: "999 Peachtree St NE #140, Atlanta",
+    key: "ChIJk1JQhkIE9YgRszI7YFGNgtQ",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/104111246635874032234\">ZAGAT</a>']"
+  },
+  {
     name: "Monday Night Brewing",
     address: "670 Trabert Ave NW, Atlanta",
     key: "ChIJGaB3peUE9YgRnAlaZJyZO9g",
     image_attribution:
       "['<a href=\"https://maps.google.com/maps/contrib/108374775105419747538\">Monday Night Brewing</a>']"
-  },
-  {
-    name: "Food Terminal",
-    address: "1000 Marietta St NW #202, Atlanta",
-    key: "ChIJX7w9HaEF9YgRPR83q-zJWis",
-    image_attribution:
-      "['<a href=\"https://maps.google.com/maps/contrib/113930873037724142825\">Torrey Smith</a>']"
   }
 ];
-export default AtlPlaces;
+export default SFPlaces;
