ec24c8cef18aa6a18ad5c7392e745d4a7c4b59a8
Terri Chu <terri.chu.dev@gmail.com>
2020-03-28 12:13:59 -0400

    new places

--- src/CityData/Places.js
+++ src/CityData/Places.js
@@ -1,165 +1,374 @@
 const AtlPlaces = [
   {
-    name: "Public School 404",
-    address: "930 Howell Mill Rd, Atlanta",
-    key: "ChIJ____aJME9YgR_gyktYS9sa0",
-    image_attribution: "['REbranding 360']"
+    name: "The Masquerade",
+    address: "50 Lower Alabama Street, Atlanta",
+    key: "ChIJ1R3_hhoE9YgR76S4q-sAxgI",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/114437781805505253972\">The Masquerade</a>']"
   },
   {
-    name: "The Optimist",
-    address: "914 Howell Mill Rd, Atlanta",
-    key: "ChIJr9PpEZME9YgRYg1YifCbOw8",
-    image_attribution: "['The Optimist']"
+    name: "Atwoods Pizza Cafe",
+    address: "817 W Peachtree St NW, Atlanta",
+    key: "ChIJlVIoR2YE9YgR8vJPLFCZPQg",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/112305291456270551461\">Atwoods Pizza Cafe</a>']"
   },
   {
     name: "Bold Monk Brewing Co.",
     address: "1737 Ellsworth Industrial Blvd NW suite d-1, Atlanta",
     key: "ChIJT9KvknAF9YgReD6o6LCcvrk",
-    image_attribution: "['Candice Boothe']"
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/103088049248843583183\">Candice Boothe</a>']"
   },
   {
-    name: "Food Terminal",
-    address: "1000 Marietta St NW UNIT 202, Atlanta",
-    key: "ChIJX7w9HaEF9YgRPR83q-zJWis",
-    image_attribution: "['Torrey Smith']"
+    name: "Papi's Cuban & Caribbean Grill",
+    address: "EMORY POINT, 1540 Avenue Pl, Atlanta",
+    key: "ChIJ75bAsVgG9YgRzbAeF_E7wy4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/114752037141507840937\">Papi&#39;s Cuban &amp; Caribbean Grill</a>']"
   },
   {
-    name: "Bocado",
-    address: "887 Howell Mill Rd NW, Atlanta",
-    key: "ChIJp1zzA5ME9YgRAnTbCVEQNJo",
-    image_attribution: "['Mark McElroy']"
+    name: "103 West",
+    address: "103 West Paces Ferry Rd NW, Atlanta",
+    key: "ChIJt6YoMY8F9YgRWkidpQqTkTk",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/107927247451862894022\">103 West</a>']"
   },
   {
-    name: "Empire State South",
-    address: "999 Peachtree St NE #140, Atlanta",
-    key: "ChIJk1JQhkIE9YgRszI7YFGNgtQ",
-    image_attribution: "['ZAGAT']"
+    name: "Jalisco Restaurant",
+    address: "2907, 2337 Peachtree Rd, Atlanta",
+    key: "ChIJfaQ6YaMF9YgRXWHnVdHufG4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/104111246635874032234\">ZAGAT</a>']"
   },
   {
-    name: "Atlanta Brewing Company",
-    address: "2323 Defoor Hills Rd NW, Atlanta",
-    key: "ChIJlzkMXDIF9YgRLRJaA2-T5aQ",
-    image_attribution: "['Atlanta Brewing Company']"
+    name: "Gaja Korean Bar",
+    address: "491 Flat Shoals Ave SE A, Atlanta",
+    key: "ChIJDesFeFsB9YgRwTS98Pt3F-g",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/117104677751800290197\">Peto Fallas</a>']"
   },
   {
-    name: "Manuel's Tavern",
-    address: "602 North Highland Avenue Northeast, Atlanta",
-    key: "ChIJnbi7oqcG9YgRCp6paNceye4",
-    image_attribution: "['Mary C Pindar']"
-  },
-  {
-    name: "Bottle Rocket Fine Food and Beverage",
-    address: "180 Walker St SW, Atlanta",
-    key: "ChIJJQEcinkD9YgRw46X2vrQ7rA",
-    image_attribution: "['Bottle Rocket Fine Food and Beverage']"
-  },
-  {
-    name: "South City Kitchen Avalon",
-    address: "9000 Avalon Way, Alpharetta",
-    key: "ChIJ0bgV0IZ19YgR4Y-xEfJ-SUg",
-    image_attribution: "['South City Kitchen Avalon']"
-  },
-  {
-    name: "South City Kitchen",
-    address: "1675 Cumberland Pkwy SE, Smyrna",
-    key: "ChIJW5BSBnca9YgRdlUsuOHPLS4",
-    image_attribution: "['South City Kitchen']"
-  },
-  {
-    name: "Henry's Louisiana Grill",
-    address: "4835 N Main St, Acworth",
-    key: "ChIJKdyidcZA9YgRtKIkEdL2PrM",
-    image_attribution: "['Michael Tharp']"
-  },
-  {
-    name: "BRIX On Main",
-    address: "4843 N Main St, Acworth",
-    key: "ChIJ1fgQ4MVA9YgRbbAWxn8b_S4",
-    image_attribution: "['BRIX On Main']"
-  },
-  {
-    name: "Alma Cocina Buckhead",
-    address: "Terminus 100, 3280 Peachtree Rd NE, Atlanta",
-    key: "ChIJIxYi-KYF9YgRGqHsohBT9pw",
-    image_attribution: "['Alexander Stanley']"
-  },
-  {
-    name: "Alma Cocina Downtown",
-    address: "191 Peachtree St NE, Atlanta",
-    key: "ChIJSd4kc3gE9YgRq6HkvZcW-cw",
-    image_attribution: "['Alma Cocina']"
+    name: "Wrecking Bar Brewpub",
+    address: "292 Moreland Ave NE, Atlanta",
+    key: "ChIJiQyZ4qsG9YgRP_wcRJGZxaw",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115027286096822577705\">Zed Star</a>']"
   },
   {
     name: "Bell Street Burritos",
     address: "112 Krog St NE #1A, Atlanta",
     key: "ChIJQab4HfgD9YgRSiCedOs3fi0",
-    image_attribution: "['Bell Street Burritos']"
-  },
-  {
-    name: "The Original El Taco",
-    address: "1186 North Highland Avenue Northeast, Atlanta",
-    key: "ChIJ-Yl0DScE9YgRnCACsvCTuLo",
-    image_attribution: "['The Original El Taco']"
-  },
-  {
-    name: "Lure",
-    address: "1106 Crescent Ave NE, Atlanta",
-    key: "ChIJWba4QUME9YgR4OgI_SW1htk",
-    image_attribution: "['Lure']"
-  },
-  {
-    name: "La Tavola",
-    address: "992 Virginia Ave NE, Atlanta",
-    key: "ChIJiSBpjyAE9YgRhD5_PW5jodY",
-    image_attribution: "['La Tavola']"
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/118421123702432324444\">Bell Street Burritos</a>']"
   },
   {
     name: "Food Terminal",
-    address: "5000 Buford Hwy NE, Chamblee",
-    key: "ChIJ3Z_V18oJ9YgRbo2xqmkKIAw",
-    image_attribution: "['Food Terminal']"
+    address: "1000 Marietta St NW UNIT 202, Atlanta",
+    key: "ChIJX7w9HaEF9YgRPR83q-zJWis",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113930873037724142825\">Torrey Smith</a>']"
   },
   {
-    name: "Pijiu Belly",
-    address: "678 10th St NW, Atlanta",
-    key: "ChIJBedpKZME9YgR3VqRCKLPLTI",
-    image_attribution: "['Alex Moffitt']"
+    name: "Bocado",
+    address: "887 Howell Mill Rd NW, Atlanta",
+    key: "ChIJp1zzA5ME9YgRAnTbCVEQNJo",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/100165444562403253362\">Mark McElroy</a>']"
   },
   {
-    name: "Ecco Buckhead",
-    address: "3586 Peachtree Rd NE, Atlanta",
-    key: "ChIJAeaqZVEP9YgR1p187fXkPGo",
-    image_attribution: "['Jon W. Lee']"
-  },
-  {
-    name: "South City Kitchen Buckhead",
-    address: "3350 Peachtree Rd #175, Atlanta",
-    key: "ChIJ6xl-kVgP9YgR063dIyw9krE",
-    image_attribution: "['Cary Charles']"
-  },
-  {
-    name: "Monday Night Brewing",
-    address: "670 Trabert Ave NW, Atlanta",
-    key: "ChIJGaB3peUE9YgRnAlaZJyZO9g",
-    image_attribution: "['Monday Night Brewing']"
-  },
-  {
-    name: "South City Kitchen",
-    address: "1144 Crescent Ave NE, Atlanta",
-    key: "ChIJvQqtX0ME9YgR-X6ukvNSt6I",
-    image_attribution: "['South City Kitchen Midtown']"
+    name: "Empire State South",
+    address: "999 Peachtree St NE #140, Atlanta",
+    key: "ChIJk1JQhkIE9YgRszI7YFGNgtQ",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/104111246635874032234\">ZAGAT</a>']"
   },
   {
     name: "Ecco Midtown",
     address: "40 7th St NE, Atlanta",
     key: "ChIJlyVS2WcE9YgR2EM86Kf9aRY",
-    image_attribution: "['Ecco Midtown']"
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/104758677764406300354\">Ecco Midtown</a>']"
+  },
+  {
+    name: "Public School 404",
+    address: "930 Howell Mill Rd, Atlanta",
+    key: "ChIJ____aJME9YgR_gyktYS9sa0",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/105849776590165710548\">REbranding 360</a>']"
+  },
+  {
+    name: "Little Trouble",
+    address: "1170 Howell Mill Rd NW Suite P-10C, Atlanta",
+    key: "ChIJdUSsCuwE9YgRCnnLP2CMuNA",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115923417741498777234\">Guillaume Sabbe</a>']"
+  },
+  {
+    name: "Chops Lobster Bar",
+    address: "70 West Paces Ferry Rd NW, Atlanta",
+    key: "ChIJ01dhwo4F9YgRyv35vhu5k10",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/109390406391415981505\">Chops Lobster Bar</a>']"
+  },
+  {
+    name: "Lure",
+    address: "1106 Crescent Ave NE, Atlanta",
+    key: "ChIJWba4QUME9YgR4OgI_SW1htk",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/118298360395548271888\">Lure</a>']"
+  },
+  {
+    name: "The Optimist",
+    address: "914 Howell Mill Rd, Atlanta",
+    key: "ChIJr9PpEZME9YgRYg1YifCbOw8",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/117726245392782967799\">The Optimist</a>']"
+  },
+  {
+    name: "F & B",
+    address: "3630 Peachtree Rd NE Ste 160, Atlanta",
+    key: "ChIJPdtczlMP9YgRlBLD-voTjYw",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/104987442817820464896\">F &amp; B</a>']"
+  },
+  {
+    name: "South City Kitchen Avalon",
+    address: "9000 Avalon Way, Alpharetta",
+    key: "ChIJ0bgV0IZ19YgR4Y-xEfJ-SUg",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/117786010096497549592\">South City Kitchen Avalon</a>']"
+  },
+  {
+    name: "Nino's Italian Restaurant",
+    address: "1931 Cheshire Bridge Rd NE, Atlanta",
+    key: "ChIJNRziKtEF9YgRl8-3Otc21DQ",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115447825260497039122\">Nino&#39;s Italian Restaurant</a>']"
+  },
+  {
+    name: "Henry's Louisiana Grill",
+    address: "4835 N Main St, Acworth",
+    key: "ChIJKdyidcZA9YgRtKIkEdL2PrM",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115766151085044387584\">Michael Tharp</a>']"
+  },
+  {
+    name: "BRIX On Main",
+    address: "4843 N Main St, Acworth",
+    key: "ChIJ1fgQ4MVA9YgRbbAWxn8b_S4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113552490500492923681\">BRIX On Main</a>']"
+  },
+  {
+    name: "South City Kitchen",
+    address: "1675 Cumberland Pkwy SE, Smyrna",
+    key: "ChIJW5BSBnca9YgRdlUsuOHPLS4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/101916169806903256445\">South City Kitchen</a>']"
+  },
+  {
+    name: "Monday Night Brewing",
+    address: "670 Trabert Ave NW, Atlanta",
+    key: "ChIJGaB3peUE9YgRnAlaZJyZO9g",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/108374775105419747538\">Monday Night Brewing</a>']"
+  },
+  {
+    name: "South City Kitchen",
+    address: "1144 Crescent Ave NE, Atlanta",
+    key: "ChIJvQqtX0ME9YgR-X6ukvNSt6I",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/105452803041468344781\">South City Kitchen Midtown</a>']"
+  },
+  {
+    name: "Pijiu Belly",
+    address: "678 10th St NW, Atlanta",
+    key: "ChIJBedpKZME9YgR3VqRCKLPLTI",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/108513545042510219877\">Alex Moffitt</a>']"
+  },
+  {
+    name: "Atlanta Brewing Company",
+    address: "2323 Defoor Hills Rd NW, Atlanta",
+    key: "ChIJlzkMXDIF9YgRLRJaA2-T5aQ",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113040785979078086482\">Atlanta Brewing Company</a>']"
+  },
+  {
+    name: "Food Terminal",
+    address: "5000 Buford Hwy NE, Chamblee",
+    key: "ChIJ3Z_V18oJ9YgRbo2xqmkKIAw",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/111259680104747944055\">Food Terminal</a>']"
+  },
+  {
+    name: "Alma Cocina Buckhead",
+    address: "Terminus 100, 3280 Peachtree Rd NE, Atlanta",
+    key: "ChIJIxYi-KYF9YgRGqHsohBT9pw",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/106389925909752099783\">Alexander Stanley</a>']"
+  },
+  {
+    name: "Cameli's Pizza",
+    address: "337 Moreland Ave NE, Atlanta",
+    key: "ChIJ5-wSta4G9YgRn3P4nexUgzA",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/111626101798823803259\">Earnest Jackson</a>']"
   },
   {
     name: "Cypress Street Pint & Plate",
     address: "817 W Peachtree St NW, Atlanta",
     key: "ChIJweczQWYE9YgRvtwgBTj9cUU",
-    image_attribution: "['Cypress Street Pint & Plate']"
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115429723269808014462\">Cypress Street Pint &amp; Plate</a>']"
+  },
+  {
+    name: "Momonoki",
+    address: "95 8th St NW #100 A, Atlanta",
+    key: "ChIJt3qYlhAF9YgRP0MHBbUh6Nw",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/110180568289867299131\">Momonoki</a>']"
+  },
+  {
+    name: "Dancing Goats Coffee Bar",
+    address: "33 Peachtree Pl NE, Atlanta",
+    key: "ChIJsZEJ9mcE9YgRDTcnhgVcVaI",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/100744712464792480931\">Joshua Kaminski</a>']"
+  },
+  {
+    name: "Ecco Buckhead",
+    address: "3586 Peachtree Rd NE, Atlanta",
+    key: "ChIJAeaqZVEP9YgR1p187fXkPGo",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/110018014909823311143\">Jon W. Lee</a>']"
+  },
+  {
+    name: "Java Cats Cafe",
+    address: "415 Memorial Dr SE ste c ste c, Atlanta",
+    key: "ChIJQ0hUXu0D9YgRIzqdeXnn4QE",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/109783719836330781046\">Java Cats Cafe</a>']"
+  },
+  {
+    name: "Alma Cocina Downtown",
+    address: "191 Peachtree St NE, Atlanta",
+    key: "ChIJSd4kc3gE9YgRq6HkvZcW-cw",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/111081873940202647414\">Alma Cocina</a>']"
+  },
+  {
+    name: "South City Kitchen Buckhead",
+    address: "3350 Peachtree Rd #175, Atlanta",
+    key: "ChIJ6xl-kVgP9YgR063dIyw9krE",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115194319650143969817\">Cary Charles</a>']"
+  },
+  {
+    name: "Banshee",
+    address: "1271 Glenwood Ave SE, Atlanta",
+    key: "ChIJeTP7sxsB9YgRxlN9qnOSUgE",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/107281069790519684993\">Lanie Weathers</a>']"
+  },
+  {
+    name: "King of Pops Beach Bar",
+    address: "Ponce City Market, 675 Ponce De Leon Ave NE #145, Atlanta",
+    key: "ChIJIxmgj4AF9YgR7g5afAt-8V4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/103870297920858767528\">King of Pops Beach Bar</a>']"
+  },
+  {
+    name: "Manuel's Tavern",
+    address: "602 North Highland Avenue Northeast, Atlanta",
+    key: "ChIJnbi7oqcG9YgRCp6paNceye4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/114422700404552250864\">Mary C Pindar</a>']"
+  },
+  {
+    name: "The Original El Taco",
+    address: "1186 North Highland Avenue Northeast, Atlanta",
+    key: "ChIJ-Yl0DScE9YgRnCACsvCTuLo",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115123136013983937509\">The Original El Taco</a>']"
+  },
+  {
+    name: "La Tavola",
+    address: "992 Virginia Ave NE, Atlanta",
+    key: "ChIJiSBpjyAE9YgRhD5_PW5jodY",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/114798838727191768782\">La Tavola</a>']"
+  },
+  {
+    name: "Bottle Rocket Fine Food and Beverage",
+    address: "180 Walker St SW, Atlanta",
+    key: "ChIJJQEcinkD9YgRw46X2vrQ7rA",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/117034873783845675454\">Bottle Rocket Fine Food and Beverage</a>']"
+  },
+  {
+    name: "Holy Taco",
+    address: "1314 Glenwood Ave SE, Atlanta",
+    key: "ChIJXW0GW1sB9YgRAg8Pv7ekF-g",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/118291458166910221682\">Julian Miguel</a>']"
+  },
+  {
+    name: "Edgewood Corner Tavern",
+    address: "464 Edgewood Ave SE, Atlanta",
+    key: "ChIJlcKQV_QD9YgRe_wjuQxP4KY",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113083175874488646632\">Edgewood Corner Tavern</a>']"
+  },
+  {
+    name: "Elmyr",
+    address: "1091 Euclid Ave NE, Atlanta",
+    key: "ChIJWW1dcakG9YgRoZwfGanhMkY",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/109827179621067937960\">Elizabeth Crosby</a>']"
+  },
+  {
+    name: "Euclid Avenue Yacht Club",
+    address: "1136 Euclid Ave NE, Atlanta",
+    key: "ChIJjf_UI6kG9YgR7p8jPDcU7uY",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/117322765835560347561\">Drew Renton</a>']"
+  },
+  {
+    name: "The Elder Tree Public House",
+    address: "469 Flat Shoals Ave SE, Atlanta",
+    key: "ChIJOUsvn1sB9YgR8SX5jx8shxs",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/114387266384952009937\">The Elder Tree Public House</a>']"
+  },
+  {
+    name: "The Basement",
+    address: "1245 Glenwood Ave SE, Atlanta",
+    key: "ChIJieVKh1sB9YgRv-f3GEMm-J0",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115453028258539039225\">Elle Pee</a>']"
+  },
+  {
+    name: "Joe's East Atlanta Coffee Shop",
+    address: "510 Flat Shoals Ave SE, Atlanta",
+    key: "ChIJX1pYZFsB9YgRbNKJr9Juw58",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/115927423287828350194\">Jason Sargis</a>']"
+  },
+  {
+    name: "Grant Central Pizza East",
+    address: "1279 Glenwood Ave SE, Atlanta",
+    key: "ChIJA2yXYlsB9YgRLx4rmMFjl-4",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/113930873037724142825\">Torrey Smith</a>']"
+  },
+  {
+    name: "The Midway Pub",
+    address: "552 Flat Shoals Ave SE, Atlanta",
+    key: "ChIJn8y6qFwB9YgR595KQhQFNDc",
+    image_attribution:
+      "['<a href=\"https://maps.google.com/maps/contrib/104372336283933877175\">Andrew Schneider</a>']"
   }
 ];
 export default AtlPlaces;
